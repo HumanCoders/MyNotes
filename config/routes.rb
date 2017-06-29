55631408a760cdd31a5a5e3b728342ca81c8def4
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-29 09:33:54 +0200

    Utilisation de helpers d'url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#hello'
-  get "welcome/hello/:message", to: "welcome#hello"
-  get "welcome/goodbye", to: "welcome#goodbye"
+  get "welcome/hello/:message", to: "welcome#hello", as: :hello_welcome
+  get "welcome/goodbye", to: "welcome#goodbye", as: :goodbye_welcome
 end
