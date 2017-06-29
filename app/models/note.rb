b22431f66ba1b09c2a86f82951079fd4c7cd6ee3
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-29 12:16:44 +0200

    Implémentation de la méthode de recherche

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,5 @@
 class Note < ApplicationRecord
+  def self.search(keywords)
+    where('lower(title) like :keywords OR lower(content) like :keywords', keywords: "%#{keywords.downcase}%")
+  end
 end
