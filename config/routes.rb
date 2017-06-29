2f470f11efaf3527bc8f33e494e9f691579acf3e
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-29 10:33:14 +0200

    Afficher la liste des notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#hello'
+  root 'notes#index'
   get "welcome/hello/(:message)", to: "welcome#hello", as: :hello_welcome
   get "welcome/goodbye", to: "welcome#goodbye", as: :goodbye_welcome
 end
