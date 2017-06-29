388c10d1106ec786b089e4dc9d7740d0d6584d79
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-29 10:36:04 +0200

    Afficher une note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,8 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def show
+    @note = Note.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'notes#index'
+
+  get "notes", to: "notes#index", as: :notes
+  get "notes/:id", to: "notes#show", as: :note
+
   get "welcome/hello/(:message)", to: "welcome#hello", as: :hello_welcome
   get "welcome/goodbye", to: "welcome#goodbye", as: :goodbye_welcome
 end
