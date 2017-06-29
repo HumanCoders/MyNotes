96eae8cd0244bdb500af14a4cdb0c11d81951aee
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-29 12:10:53 +0200

    Utilisation de "resources" dans les routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'notes#index'
 
-  get "notes", to: "notes#index", as: :notes
-  get "notes/new", to: "notes#new", as: :new_note
-  post "notes", to: "notes#create"
-  get "notes/:id/edit", to: "notes#edit", as: :edit_note
-  patch "notes/:id", to: "notes#update"
-  get "notes/:id", to: "notes#show", as: :note
-  delete "notes/:id", to: "notes#destroy"
+  resources :notes
 
   get "welcome/hello/(:message)", to: "welcome#hello", as: :hello_welcome
   get "welcome/goodbye", to: "welcome#goodbye", as: :goodbye_welcome
